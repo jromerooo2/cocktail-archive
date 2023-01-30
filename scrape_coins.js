96f292fb6ba849d04be2b505e179637b37c8324a
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2023-01-30 17:37:10 -0600

    update: functions

--- scrape_coins.js
+++ scrape_coins.js
@@ -4,21 +4,24 @@
 
 */
 
-//script created for Mr.Fasse
-
-function myFunction() {
-  let arr_data = scrape("casper")
-  Logger.log("Current price: " + arr_data[0]);
-  Logger.log("Percent: " + arr_data[1]);
+function main() {
+  let arr_data = scrape_coin("casper")
+  add_coin(arr_data);
+  Logger.log("Finished.")
 }
 
-function scrape(coin){
+function add_coin(data) {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  sheet.appendRow([data[0],data[1]]);
+}
+
+function scrape_coin(coin){
   //using cryptorank.io webscrapping since coinmarketcap blocks requests for google
   var webURL = "https://cryptorank.io/price/"+coin;
   var response = UrlFetchApp.fetch(webURL);
   var $ = Cheerio.load(response.getContentText()); 
   let price = $('.table-cell-with-update__CurrencyWrapper-sc-crypsw-0').first().text().trim();
   let percent = $('.percent').first().text().trim();
-  
+
   return [price,percent];
 }
